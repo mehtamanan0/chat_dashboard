96c2f5f866495d56a3290b850f815d19b64fb2c1
manan mehta <mehtamanan0@gmail.com>
2017-06-20 14:29:55 +0530

    re added sql path

--- global.R
+++ global.R
@@ -23,7 +23,7 @@ viewCache <- function(df){
 
 ########## SQL CONNECTION ################################
 library(RSQLite)
-con <- dbConnect(SQLite(), "/home/manan/Downloads/mydb")
+con <- dbConnect(SQLite(), "/home/haptik/Downloads/mydb")
 ##########################################################
 
 

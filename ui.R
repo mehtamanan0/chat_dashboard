492b8d8cb49290850b91425ce7ae0d364cd0ca06
amansrivastava17 <amans.rlx@gmail.com>
2017-07-06 11:23:14 +0530

    icon changes

--- ui.R
+++ ui.R
@@ -5,8 +5,8 @@ dashboardPage(skin = "black",
               dbHeader,
               dashboardSidebar(
                 sidebarMenu(
-                  menuItem("Channel Bot-Stats", tabName = "haptik", icon = icon("cloud")),
-                  menuItem("Break Messages", tabName = "break", icon = icon("cloud")),
+                  menuItem("Channel Bot-Stats", tabName = "haptik", icon = icon("signal")),
+                  menuItem("Break Messages", tabName = "break", icon = icon("list-alt")),
                   selectInput('date', 'What Happens in', multiple=FALSE, selectize=TRUE,choices = date_filters),
                   selectInput('channel', 'Select Channel', multiple=FALSE, selectize=TRUE,choices = c("flightschannel","reminderschannel"))
                 ),
